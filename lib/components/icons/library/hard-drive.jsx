32ea628e7d2c4590cb153e9d8f0e440057f2eb9a
gh-jsoares <jsoares.public@gmail.com>
2026-07-10 14:17:12 +0200

    feat: add HardDrive icon

---
+++ lib/components/icons/library/hard-drive.jsx
@@ -0,0 +1,9 @@
+import Icon from "../icon.jsx";
+
+export default function HardDrive(props) {
+  return (
+    <Icon {...props}>
+      <path d="M2 17h20v2c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2v-2zm0-2l2.8-8.4C5.1 5.6 6 5 7 5h10c1 0 1.9.6 2.2 1.6L22 15H2zm14 3a1 1 0 100 2 1 1 0 000-2zm3 0a1 1 0 100 2 1 1 0 000-2z" />
+    </Icon>
+  );
+}
