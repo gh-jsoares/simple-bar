3e72b23d47af80381d995537b065190efbd9a8ce
gh-jsoares <jsoares.public@gmail.com>
2026-07-10 16:22:28 +0200

    feat: cleaner charging bolt icon, solid fill, better proportioned

--- lib/components/icons/library/charging.jsx
+++ lib/components/icons/library/charging.jsx
@@ -2,8 +2,8 @@ import Icon from "../icon.jsx";
 
 export default function Charging(props) {
   return (
-    <Icon {...props}>
-      <path d="M7 2v11h3v9l7-12h-4l3-8z" />
+    <Icon {...props} viewBox="0 0 12 12">
+      <path d="M7 0L3 7h3l-1 5 5-7H7l1-5z" />
     </Icon>
   );
 }
